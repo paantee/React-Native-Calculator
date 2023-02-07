a966aae80708f838e3bc56da7dea0fa74cbb41b1
Teemu Paananen <teemu-paananen@hotmail.com>
2023-02-07 15:58:11 +0200

    navigation

--- App.js
+++ App.js
@@ -1,16 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, Alert, TextInput } from 'react-native';
 import React from 'react';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Calculator from './assignments/calculator';
 import Guess from './assignments/guess';
 
-export default function App() {
-  
+const Tab = createBottomTabNavigator();
 
+export default function App() {
   return (
-    
-      <Calculator/>
-    
+    <NavigationContainer>
+      <Tab.Navigator
+      screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Calculator') {
+              iconName = focused
+                ? 'calculator'
+                : 'calculator';
+            } else if (route.name === 'Guess Game') {
+              iconName = focused
+                ? 'game-controller'
+                : 'game-controller';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'green',
+          tabBarInactiveTintColor: 'gray',
+        })}>
+        <Tab.Screen name="Calculator" component={Calculator} />
+        <Tab.Screen name="Guess Game" component={Guess} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
